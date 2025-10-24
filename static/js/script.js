0ad07e4bf842ca42cf1e9dab130bca5563662d49
johnArastoozad <johna@ocsx.com>
2025-10-24 08:14:54 -0700

    script change

--- static/js/script.js
+++ static/js/script.js
@@ -11,6 +11,7 @@ function sendMail(){
     emailjs
         .send("service_ut8yjbw", "template_604bgh2", parms)
         .then(alert("Email Sent Successfully. Thank you!").catch(()=>alert("Email not sent!!")))
-        .then(window.location.href='index.html');
+        .then(window.location.href='index.html');\
+    window.location.href='index.html'
 
 }
